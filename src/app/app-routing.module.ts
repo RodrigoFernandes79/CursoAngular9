0c2a88259e9df21648ee5612ac627921b2956ca0
RodrigoFernandes79 <rodrigohf79@hotmail.com>
2021-12-03 22:17:44 -0300

    Criando primeiro componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'primeiroComponente' , component:PrimeiroComponenteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
